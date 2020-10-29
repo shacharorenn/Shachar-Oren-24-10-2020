24742a9410aba5d3ffc4eca44bb06ce8d7c6f87b
Shachar Oren <shacharorennn@gmail.com>
2020-10-29 15:12:15 +0200

    update about text and style

--- herolo-front/src/MessageController/About.js
+++ herolo-front/src/MessageController/About.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 export const About = () => {
     return(
-        <>
-        <h1>About Me</h1>
-        <p>My name is Shachar</p>
-        <p>I'm a passionate fullstack developer</p>
-        <p>that want to learn and dev</p>
-        <p>I'm motivated and a self learner </p>
-        <p>I know this assignment isn't perfect</p>
-        <p>I learned React alone and</p>
-        <p>I'm looking</p>
-        <p>a place with tough reviews</p>
-        <p>in order to be the best develop I can be </p>
-        </>
+        <div className="about-style">
+            <h1>About Me</h1>
+            <p>My name is Shachar. I'm a passionate fullstack developer that want to learn and dev. 
+                I'm motivated and a fast self-learning skills,
+                I know this assignment isn't perfect , 
+                but I'm looking a place with tough reviews in order 
+                to be the best develop I can be.</p>
+        </div>
     )
 }
